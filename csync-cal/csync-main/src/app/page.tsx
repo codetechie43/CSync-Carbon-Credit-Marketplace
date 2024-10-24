c30eea7ec942e668032953e0589ff07052f7fc85
codetechie43 <sujalkhade676@gmail.com>
2024-10-25 04:26:12 +0530

    footprint calculator

---
+++ csync-cal/csync-main/src/app/page.tsx
@@ -0,0 +1,9 @@
+import CarbonFootprintCalculator from "../components/carbon-footprint-calculator";
+
+export default function Home() {
+  return (
+    <>
+      <CarbonFootprintCalculator />
+    </>
+  );
+}
